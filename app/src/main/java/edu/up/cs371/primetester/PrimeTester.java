c0502df5dd19a985cb8e80e55be001a3e2b1e4e9
jablonsk19 <jablonsk19@up.edu>
2016-08-31 17:09:12 -0700

    Working Prime Tester (not as efficient as I would normally do, but whatever)

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -14,7 +14,15 @@ public class PrimeTester {
      * @return true iff n is prime
      */
     public static boolean isPrime(long n) {
-        // for now, return a random result
-        return Math.random() > 0.5;
+        for(long i = 2; i <= Math.sqrt(n); i++)
+        {
+            if(n%i == 0)
+                return false;
+        }
+
+        if(n == 0 || n == 1)
+            return false;
+        //else
+        return true;
     }
 }
